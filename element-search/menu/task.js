0cf884db9f55cb3698892b39ca4ae70d0ceac9c1
Nataliya-grish <gray-wings@yandex.ru>
2022-10-22 18:09:02 +0300

    done homework

---
+++ element-search/menu/task.js
@@ -0,0 +1,18 @@
+const menuLink = document.querySelectorAll(".menu__link");
+[...menuLink].forEach((item) => {
+	item.onclick = () => {
+		const menuSub = item.closest(".menu__item").querySelector(".menu_sub");
+		if (menuSub) {
+			if (!menuSub.classList.contains("menu_active")) {
+				const menuActive = item.closest(".menu_main").querySelectorAll(".menu_active");
+				if (menuActive.length > 0) {
+					[...menuActive].forEach((item) => item.classList.remove("menu_active"));
+				};
+				menuSub.classList.add("menu_active");
+			} else {
+				menuSub.classList.remove("menu_active");
+			};
+			return false;
+		};
+	}
+});
